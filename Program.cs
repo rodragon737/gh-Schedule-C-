9c2b88e720306f37416b8544f6c668357a7213fe
Rodrigo Martin Rosales <rosales.rodrigo.martin@gmail.com>
2023-07-10 18:35:27 -0300

    Conteo index de tareas

--- Program.cs
+++ Program.cs
@@ -57,14 +57,11 @@ namespace ToDo
                 string line = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1)
-                {
-                    if (TaskList.Count > 0)
-                    {
+                if (indexToRemove > -1 && TaskList.Count > 0)
+                {                 
                         string task = TaskList[indexToRemove];
                         TaskList.RemoveAt(indexToRemove);
                         Console.WriteLine("Tarea " + task + " eliminada");
-                    }
                 }
             }
             catch (Exception)
@@ -89,11 +86,9 @@ namespace ToDo
         public static void ShowList()
         {
                Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+               var indexTask=1;
+               TaskList.ForEach(p=> Console.WriteLine(indexTask++ + ". " + p));
+               Console.WriteLine("----------------------------------------");
         }
 
         public static void ShowMenuTaskList()
